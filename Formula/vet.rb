9f4e44887290dd189c88e80901df9cd3c7e3a61a
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-03-01 23:55:29 +0100

    Add formula

---
+++ Formula/vet.rb
@@ -0,0 +1,19 @@
+ class Vet < Formula
+  desc "Gerrit client using pull request review workflow"
+  homepage "https://github.com/Cosium/vet"
+  url "https://cdn.rawgit.com/Cosium/homebrew-vet/6e55c992/1.0/macosx_x64/vet.zip"
+  sha256 "89af9a7911a861198cb602afcb28994f8dee6d4d4ff2214b1cb64f5534b076ec"
+  def install
+    prefix.install Dir["*"]
+    system "mv", "#{prefix}/bin", "#{prefix}/bins"
+    system "mkdir", "#{prefix}/bin"
+    File.open("#{prefix}/bin/vet", "w") { |f|
+	f << "#! /bin/sh\n"
+        f << "#{prefix}/bins/vet $@"
+    }
+  end
+   test do
+    system "#{bin}/vet", "--version"
+  end
+end
+
