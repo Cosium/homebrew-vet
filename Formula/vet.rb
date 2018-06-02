5a545554c9794ab94c1c9dbef52372ad47b518c6
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-06-02 14:49:13 +0200

    Release 3.15

--- Formula/vet.rb
+++ Formula/vet.rb
@@ -1,8 +1,8 @@
 class Vet < Formula
   desc "Gerrit client using pull request review workflow"
   homepage "https://github.com/Cosium/vet"
-  url "https://github.com/Cosium/vet/releases/download/3.14/vet-macosx_x64.zip"
-  sha256 "7f365900b8dfa955278794ee5f7a2c090751859e0a545e8b647139b86f7d6019"
+  url "https://github.com/Cosium/vet/releases/download/3.15/vet-macosx_x64.zip"
+  sha256 "cb181725833ced2fdbfaae2cfb6a38e99b31f62bd8bb3db652b6e37c4ec566a7"
   def install
     prefix.install Dir["*"]
     system "mv", "#{prefix}/bin", "#{prefix}/bins"
